bcead1b263e7056f84b86823528c3639c087e385
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-18 10:42:02 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,51 @@ import { Box, Flex, Image } from "@chakra-ui/react";
 import logoImg from "./assets/logo.png";
 import bubbleImg from "./assets/bubble.png";
 import "../global.css";
+import { useState } from "react";
+import { SetQuestionQty } from "./features/SetQuestionQty";
+
+enum Step {
+  SetQuestionQty,
+  SetQuestionCategory,
+  SetQuestionDifficulty,
+  Play,
+  Score,
+}
 export function App() {
+  const [step, setStep] = useState<Step>(Step.SetQuestionQty);
   const header = (
     <Flex justify="center">
-      <Image h={"24"} src={logoImg} />
+      <Image h="24" src={logoImg} />
     </Flex>
   );
 
+  const renderScreenByStep = () => {
+    switch (step) {
+      case Step.SetQuestionQty:
+        return <SetQuestionQty />;
+      case Step.SetQuestionCategory:
+        return <></>;
+      case Step.SetQuestionDifficulty:
+        return <></>;
+      case Step.Play:
+        return <></>;
+      case Step.Score:
+        return <></>;
+      default:
+        return null;
+    }
+  };
   return (
     <Box py={"10"} h="100%">
       {header}
       <Image
         src={bubbleImg}
+        position={"absolute"}
         zIndex={-1}
-        position="absolute"
         right={-120}
         top={100}
       />
-      <Box mt={100}>Todo</Box>
+      <Box>{renderScreenByStep()}</Box>
     </Box>
   );
 }
