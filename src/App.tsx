1bb08ad208d9a5c2e82b73eabaa07b21717a6370
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-18 10:13:04 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-import { Heading } from "@chakra-ui/react";
-
+import { Box, Flex, Image } from "@chakra-ui/react";
+import logoImg from "./assets/logo.png";
+import bubbleImg from "./assets/bubble.png";
 export function App() {
-  return <Heading>Hello</Heading>;
+  const header = (
+    <Flex justify="center">
+      <Image h={"24"} src={logoImg} />
+    </Flex>
+  );
+
+  return (
+    <Box py={"10"} h="100%">
+      {header}
+      <Image
+        src={bubbleImg}
+        zIndex={-1}
+        position="absolute"
+        right={-120}
+        top={100}
+      />
+      <Box mt={100}>Todo</Box>
+    </Box>
+  );
 }
