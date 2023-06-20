c8aed5ab6679f21ab25e228977b30608f6cfcffe
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-20 18:30:19 +0200

    wip

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -3,32 +3,30 @@ import { QuizItem } from "../types/quiz-type";
 import {
   Flex,
   Heading,
+  Radio,
   RadioGroup,
   SimpleGrid,
-  Radio,
   Text,
 } from "@chakra-ui/react";
 
 export function PlayQuiz(p: { quiz: QuizItem[] }) {
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
-  const availableAnswers = [
+  const availableAnswers: string[] = [
     currentQuizItem.correct_answer,
     ...currentQuizItem.incorrect_answers,
   ];
 
-  const radioList = availableAnswers.map((answer: string, i: number) => {
+  const radioList = availableAnswers.map((availableAnswer: string) => {
     return (
-      <Radio key={i} value={answer}>
-        <Text dangerouslySetInnerHTML={{ __html: answer }} />
+      <Radio key={availableAnswer} value={availableAnswer}>
+        <Text dangerouslySetInnerHTML={{ __html: availableAnswer }}></Text>
       </Radio>
     );
   });
-
   return (
-    <Flex direction={"column"} alignItems={"center"} justify={"center"} px={5}>
+    <Flex direction={"column"} alignItems={"center"} justify={"center"}>
       <Heading
-        as="h1"
         fontSize={"3xl"}
         mt={100}
         mb={20}
@@ -36,8 +34,8 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
       />
 
       <RadioGroup
-        onChange={(answer) => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}
         value={""}
+        onChange={() => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}
       >
         <SimpleGrid columns={2} spacing={4}>
           {radioList}
