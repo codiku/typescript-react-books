c72847de81e1539ea63e7affc987e522c52422c9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-21 12:55:43 +0200

    wip

--- src/features/PlayQuiz.tsx
+++ src/features/PlayQuiz.tsx
@@ -18,20 +18,10 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
   const [answer, setAnswer] = useState<string>();
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
-  const [history, setHistory] = useState<boolean[]>([]);
   const [questionStatus, setQuestionStatus] = useState<
     "valid" | "invalid" | "unanswered"
   >("unanswered");
   const [availableAnswers, setAvailableAnswers] = useState<string[]>([]);
-
-  useEffect(() => {
-    setAvailableAnswers(
-      [
-        currentQuizItem.correct_answer,
-        ...currentQuizItem.incorrect_answers,
-      ].sort(() => Math.random() - 0.5)
-    );
-  }, [currentQuizItemIndex]);
 
   useEffect(() => {
     setAvailableAnswers(
@@ -50,35 +40,11 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
       } else {
         setQuestionStatus("invalid");
       }
-      setHistory([...history, isValid]);
     }
   }, [answer]);
 
   const isValidAnswer = (answer: string): boolean => {
     return answer === currentQuizItem.correct_answer;
-  };
-
-  const progressBar = () => {
-    return (
-      <HStack>
-        {p.quiz.map((_, i) => {
-          return (
-            <Box
-              key={i}
-              h={3}
-              backgroundColor={
-                i >= currentQuizItemIndex
-                  ? "gray.200"
-                  : history[i] === true
-                  ? "green.300"
-                  : "red.300"
-              }
-              w={25}
-            />
-          );
-        })}
-      </HStack>
-    );
   };
 
   const radioList = availableAnswers.map((availableAnswer: string) => {
@@ -99,7 +65,6 @@ export function PlayQuiz(p: { quiz: QuizItem[] }) {
   });
   return (
     <Flex direction={"column"} alignItems={"center"} justify={"center"}>
-      {progressBar()}
       <Heading
         fontSize={"3xl"}
         mt={100}
